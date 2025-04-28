eb3e662a0f63cb01a79c27113b082a1e180e91e6
Khuslenttt <entegshjargal1@s.sfusd.edu>
2025-04-28 09:15:24 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -8,13 +8,13 @@ public class RandomStringChooser
     for(String w : wordArray){
       words.add(w);
     }
+  }
     public String getNext(){
       if(words.size() == 0)
         return "NONE";
       int i = (int)(Math.random() * words.size());
       return words.remove(i);
     }
-  }
   //Heads up! 
   //You will get a very confusing error message until you have working code in part b as well
 }
